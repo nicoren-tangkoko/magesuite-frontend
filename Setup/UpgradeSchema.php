6776f7b5463ebac1b22e040029119589dc70507a
Krzysztof Kurzydło <krzysztof.kurzydlo@creativestyle.pl>
2018-09-17 17:22:53 +0200

    Updated setup scripts to check if column exist

--- Setup/UpgradeSchema.php
+++ Setup/UpgradeSchema.php
@@ -9,28 +9,17 @@ class UpgradeSchema implements \Magento\Framework\Setup\UpgradeSchemaInterface
         $setup->startSetup();
 
         if (version_compare($context->getVersion(), '0.0.5', '<')) {
-            $setup->getConnection()->addColumn(
-                $setup->getTable('cms_page'),
-                'multistore_identifier',
-                [
-                    'type' => \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-                    'length' => 255,
-                    'comment' => 'Identifier key for multistore'
-                ]
-            );
-        }
-
-        if (version_compare($context->getVersion(), '0.0.6', '<')) {
-            $setup->getConnection()->changeColumn(
-                $setup->getTable('cms_page'),
-                'multistore_identifier',
-                'page_group_identifier',
-                [
-                    'type' => \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-                    'length' => 255,
-                    'comment' => 'Page Group Identifier key for multistore'
-                ]
-            );
+            if ($setup->getConnection()->tableColumnExists($setup->getTable('cms_page'), 'page_group_identifier') === false) {
+                $setup->getConnection()->addColumn(
+                    $setup->getTable('cms_page'),
+                    'page_group_identifier',
+                    [
+                        'type' => \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                        'length' => 255,
+                        'comment' => 'Identifier key for multistore'
+                    ]
+                );
+            }
         }
 
         $setup->endSetup();
