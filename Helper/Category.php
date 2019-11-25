a4f5f4054bf7e032b319c7d401cab4282c3ce793
Blazej Doleska <blazej.doleska@creativestyle.pl>
2019-11-25 13:48:53 +0100

    changed private property to protected

--- Helper/Category.php
+++ Helper/Category.php
@@ -13,7 +13,7 @@ class Category extends \Magento\Framework\App\Helper\AbstractHelper
     /**
      * @var \Magento\Framework\Registry
      */
-    private $registry;
+    protected $registry;
 
     /**
      * @var \MageSuite\Frontend\Model\Category\Tree
