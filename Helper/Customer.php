2b42bd2a7779f0249919d95ae18383b72eddf9b8
Błażej Dołęska <blazej.doleska@creativestyle.pl>
2020-01-03 15:41:43 +0000

    changed logic of checking if customer is logged in

--- Helper/Customer.php
+++ Helper/Customer.php
@@ -5,24 +5,24 @@ namespace MageSuite\Frontend\Helper;
 class Customer extends \Magento\Framework\App\Helper\AbstractHelper
 {
     /**
-     * @var \Magento\Customer\Model\Session
-     */
-    private $session;
-
-    /**
      * @var \Magento\Framework\HTTP\PhpEnvironment\RemoteAddress
      */
     protected $remoteAddress;
 
+    /**
+     * @var \Magento\Framework\App\Http\Context
+     */
+    protected $httpContext;
+
     public function __construct(
         \Magento\Framework\App\Helper\Context $context,
-        \Magento\Customer\Model\Session $session,
-        \Magento\Framework\HTTP\PhpEnvironment\RemoteAddress $remoteAddress
+        \Magento\Framework\HTTP\PhpEnvironment\RemoteAddress $remoteAddress,
+        \Magento\Framework\App\Http\Context $httpContext
     ) {
         parent::__construct($context);
 
-        $this->session = $session;
         $this->remoteAddress = $remoteAddress;
+        $this->httpContext = $httpContext;
     }
 
     public function getCustomerIp()
@@ -32,7 +32,7 @@ class Customer extends \Magento\Framework\App\Helper\AbstractHelper
 
     public function isLoggedIn()
     {
-        return $this->session->isLoggedIn();
+        return $this->httpContext->getValue(\Magento\Customer\Model\Context::CONTEXT_AUTH);
     }
 
 }
